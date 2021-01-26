065f77b1c5306f2e8485c33d0b57bb5cbbc28f8d
Rahan Bouess <46539830+Absolu242@users.noreply.github.com>
2021-01-26 19:26:24 +0800

    The first commit
     The first commit of the Final build of Shila eCommerce

---
+++ precache-manifest.c9419e5a91eded2d51b372983203091d.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e5e1f222031bec407081b244b6ab40c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bdf1c98ded984c32a500",
+    "url": "/static/css/main.f690b76d.chunk.css"
+  },
+  {
+    "revision": "a85ce0a1b18a9ba9cde8",
+    "url": "/static/js/2.24aad71b.chunk.js"
+  },
+  {
+    "revision": "9c5a12a47758e0e78b9b23c7f2223aff",
+    "url": "/static/js/2.24aad71b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdf1c98ded984c32a500",
+    "url": "/static/js/main.465d40ac.chunk.js"
+  },
+  {
+    "revision": "a4382debbea0b6828cac",
+    "url": "/static/js/runtime-main.1c4dadcd.js"
+  },
+  {
+    "revision": "045e711e6602d3fb28548f8a05dd64a5",
+    "url": "/static/media/ShoesCat.045e711e.jpg"
+  },
+  {
+    "revision": "871508ec7f9480def811dc368936eeb2",
+    "url": "/static/media/bag1.871508ec.jpg"
+  },
+  {
+    "revision": "a06a1f03ec41591a967d6f491182df9c",
+    "url": "/static/media/bag2.a06a1f03.jpg"
+  },
+  {
+    "revision": "4e615896bc8f598cf3c7bc9dcd1291f9",
+    "url": "/static/media/bag3.4e615896.jpg"
+  },
+  {
+    "revision": "543188ab92c3af90d000e778d48e6e44",
+    "url": "/static/media/bag4.543188ab.jpg"
+  },
+  {
+    "revision": "a859afbf9fbefd91d7c337a8ad1ce317",
+    "url": "/static/media/cat1.a859afbf.jpg"
+  },
+  {
+    "revision": "5884f7017ed83fd59a939985f886b5c4",
+    "url": "/static/media/clothes1.5884f701.jpg"
+  },
+  {
+    "revision": "2d8632b00940ad5f2c98295051a515d7",
+    "url": "/static/media/clothes2.2d8632b0.jpg"
+  },
+  {
+    "revision": "aaa7c2acb147e89c031290afc2eda0ec",
+    "url": "/static/media/clothes3.aaa7c2ac.jpg"
+  },
+  {
+    "revision": "980409a3641f22afee4d83c2a4ce04ad",
+    "url": "/static/media/clothes4.980409a3.jpg"
+  },
+  {
+    "revision": "710e015f62d81c8faee7504d61ada957",
+    "url": "/static/media/clothescat1.710e015f.jpg"
+  },
+  {
+    "revision": "fdff5be0938ce85bdbb543bf116a53b0",
+    "url": "/static/media/clothescat2.fdff5be0.jpg"
+  },
+  {
+    "revision": "0ee909d86f9ead1e35adf0b47e4aab4a",
+    "url": "/static/media/main2.0ee909d8.jpg"
+  },
+  {
+    "revision": "97b81db58cfbd4c06f864eefe98f738c",
+    "url": "/static/media/map.97b81db5.jpg"
+  },
+  {
+    "revision": "23c28331ed76aba94b48b0e67f157a10",
+    "url": "/static/media/shoes1.23c28331.jpg"
+  },
+  {
+    "revision": "a40946e5a8726a4473d47930e61b70f7",
+    "url": "/static/media/shoes2.a40946e5.jpg"
+  },
+  {
+    "revision": "378a401bb999547db2df614b3fb01a2f",
+    "url": "/static/media/shoes3.378a401b.jpg"
+  },
+  {
+    "revision": "19299139991c69d1bb398ecb99da4da6",
+    "url": "/static/media/shoes4.19299139.jpg"
+  },
+  {
+    "revision": "8278e4ce12154dc49781a30f8542bcb3",
+    "url": "/static/media/shoescat2.8278e4ce.jpg"
+  },
+  {
+    "revision": "91b7c361a35ddb4fa8b3ab955ca16812",
+    "url": "/static/media/shop.91b7c361.jpg"
+  }
+]);
